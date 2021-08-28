8befc066fba2d1b3e979f25ee2d58de4ed81d8d5
Colin <colin.diesh@gmail.com>
2021-08-28 18:56:02 -0400

    Use threejs

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import { BamFile, BamRecord } from '@gmod/bam'
 import { RemoteFile, BlobFile } from 'generic-filehandle'
 import GranularRectLayout from './layout'
 import { StringParam, useQueryParams, withDefault } from 'use-query-params'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, extend, useThree, useFrame } from '@react-three/fiber'
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+// Calling extend with the native OrbitControls class from Three.js
+// will make orbitControls available as a native JSX element.
+// Notice how the OrbitControls classname becomes lowercase orbitControls when used as JSX element.
+extend({ OrbitControls })
 
 function parseLocString(locString: string) {
   const [refName, rest] = locString.split(':')
@@ -26,39 +33,65 @@ export function useForceUpdate() {
 
 const featHeight = 10
 const width = 1800
-const height = 1000
 const snpcovheight = 100
 const initialLoc = '1:20000-21000'
 const initialFile =
   'https://s3.amazonaws.com/1000genomes/phase3/data/HG00096/alignment/HG00096.mapped.ILLUMINA.bwa.GBR.low_coverage.20120522.bam'
 
 function Box(props: any) {
-  const ref = useRef<any>()
+  const { width, height, color } = props
   const [hovered, setHover] = useState(false)
   const [active, setActive] = useState(false)
   return (
     <mesh
       {...props}
-      ref={ref}
-      scale={active ? 1.5 : 1}
       onClick={() => setActive(!active)}
       onPointerOver={() => setHover(true)}
       onPointerOut={() => setHover(false)}
     >
-      <boxGeometry args={[2, 1, 0]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <planeGeometry args={[width, height]} />
+      <meshStandardMaterial
+        color={color || (active ? 'green' : hovered ? 'hotpink' : 'orange')}
+      />
     </mesh>
   )
 }
 
+const CameraControls = () => {
+  // Get a reference to the Three.js Camera, and the canvas html element.
+  // We need these to setup the OrbitControls class.
+  // https://threejs.org/docs/#examples/en/controls/OrbitControls
+
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree()
+
+  // Ref to the controls, so that we can update them on every frame using useFrame
+  const controls = useRef<any>()
+  useFrame(() => controls.current.update())
+  return (
+    //@ts-ignore
+    <orbitControls
+      ref={controls}
+      args={[camera, domElement]}
+      enableZoom={true}
+      maxAzimuthAngle={Math.PI / 3}
+      maxPolarAngle={Math.PI}
+      minAzimuthAngle={-Math.PI / 3}
+      minPolarAngle={0}
+    />
+  )
+}
+
 function App() {
-  const ref = useRef<HTMLCanvasElement>(null)
   const snpcovref = useRef<HTMLCanvasElement>(null)
   const fileRef = useRef<HTMLInputElement>(null)
   const [params, setParams] = useQueryParams({
     loc: withDefault(StringParam, initialLoc),
     file: withDefault(StringParam, initialFile),
   })
+  const [layout, setLayout] = useState<any>()
   const [readData, setReadData] = useState<BamRecord[]>()
   const [mpileupData, setMPileupData] = useState<number[]>()
   const [file, setFile] = useState(params.file)
@@ -67,11 +100,18 @@ function App() {
   const forceUpdate = useForceUpdate()
   const [files, setFiles] = useState<FileList>()
 
+  const rects = layout ? [...layout.rectangles.entries()] : undefined
+  const parsedLoc = parseLocString(params.loc)
+  const bpPerPx = (width / (parsedLoc.end - parsedLoc.start)) * 10
+
   useEffect(() => {
     ;(async () => {
       let bam
       if (files) {
-        let bamIdx = files[0].name.endsWith('bam') || files[0].name.endsWith('cram') ? 0 : 1
+        let bamIdx =
+          files[0].name.endsWith('bam') || files[0].name.endsWith('cram')
+            ? 0
+            : 1
 
         bam = new BamFile({
           bamFilehandle: new BlobFile(files[bamIdx]),
@@ -106,34 +146,14 @@ function App() {
 
   // this block draws the rectangles
   useEffect(() => {
-    if (!ref.current) {
-      return
-    }
-    const ctx = ref.current.getContext('2d')
-    if (!ctx) {
-      return
-    }
-    ctx.clearRect(0, 0, width, height)
-    const parsedLoc = parseLocString(params.loc)
-    const bpPerPx = width / (parsedLoc.end - parsedLoc.start)
     const layout = new GranularRectLayout()
     readData?.forEach((feature) => {
       const start = feature.get('start')
       const end = feature.get('end')
 
-      if (feature.isReverseComplemented()) {
-        ctx.fillStyle = '#99f'
-      } else {
-        ctx.fillStyle = '#f99'
-      }
-
-      const leftPx = (start - parsedLoc.start) * bpPerPx
-      const width = (end - start) * bpPerPx
-
-      const y = layout.addRect(feature.id(), start, end, featHeight)
-
-      ctx.fillRect(leftPx, y, width, featHeight)
+      layout.addRect(`${feature.id()}`, start, end, featHeight, feature)
     })
+    setLayout(layout)
   }, [readData, params.loc])
 
   useEffect(() => {
@@ -145,8 +165,6 @@ function App() {
       return
     }
     ctx.clearRect(0, 0, width, snpcovheight)
-    const parsedLoc = parseLocString(params.loc)
-    const bpPerPx = width / (parsedLoc.end - parsedLoc.start)
     if (mpileupData) {
       const maxHeight = mpileupData.reduce((a, b) => Math.max(a, b), 0)
       for (let i = 0; i < mpileupData.length; i++) {
@@ -161,7 +179,7 @@ function App() {
       ctx.fillStyle = 'black'
       ctx.fillText(`[0, ${maxHeight}]`, 0, 20)
     }
-  }, [mpileupData, params.loc])
+  }, [mpileupData, bpPerPx])
 
   return (
     <div>
@@ -197,18 +215,52 @@ function App() {
 
         <br />
         <label htmlFor="loc">Location: </label>
-        <input id="loc" type="text" value={loc} onChange={(event) => setLoc(event.target.value)} />
+        <input
+          id="loc"
+          type="text"
+          value={loc}
+          onChange={(event) => setLoc(event.target.value)}
+        />
         <button type="submit">Submit</button>
       </form>
       {!readData ? <div className="dots">Loading...</div> : null}
       {error ? <div style={{ color: 'red' }}>{error}</div> : null}
+      <div style={{ width: '100%', height: 800, margin: 100 }}>
+        <Canvas camera={{ position: [0, 0, 800], near: 5, far: 15000 }}>
+          <CameraControls />
+          <ambientLight />
+          {rects?.map(([key, val]) => {
+            const { minX: start, maxX: end, minY, maxY } = val
+            const left = (start - parsedLoc.start) * bpPerPx
+            const width = (end - start) * bpPerPx
+            const height = maxY - minY
+            return (
+              <Box
+                key={key}
+                width={width}
+                height={height}
+                position={[left - 900, minY + 200, 0]}
+              />
+            )
+          })}
 
-      <Canvas>
-        <ambientLight />
-        {[-45, -35, -25, -15, -5, 5, 15, 25, 35, 45].map((pos) => (
-          <Box key={pos} position={[pos, 0, 0]} />
-        ))}
-      </Canvas>
+          {mpileupData?.map((val, index) => {
+            const left = index * bpPerPx
+            const width = bpPerPx
+            const height = val * 20
+            return (
+              <Box
+                key={val + '-' + index}
+                width={width}
+                height={height}
+                color={'grey'}
+                position={[left - 900, 180 - height / 2, 0]}
+              />
+            )
+          })}
+        </Canvas>
+        ,
+      </div>
     </div>
   )
 }
